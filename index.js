a27e198dbe0019007efa7b023498895743fd41be
joaovictor1997 <victorcrf@live.com>
2022-07-08 17:33:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const app = express();
+
+const conn = require('./db/conn');
+
+const Task = require('./models/Tasks');
+
+const tasksRoutes = require('./routes/tasksRoutes');
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/tasks', tasksRoutes);
+
+
+
+conn.sync().then(() => {
+  app.listen(3000);
+}).catch((err) => console.log(err));
